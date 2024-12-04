809fb4edcab1e9ba506087580723922ee6b3b624
rish1003 <rishikamukherjee2004@gmail.com>
2024-12-04 16:00:37 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import ProductListingPage from "./pages/ProductListingPage";
+import CartPage from "./pages/CartPage";
+import Header from "./components/Header";
+import "./styles/Responsive.css";
 
-function App() {
+const App = () => {
+  const [cart, setCart] = useState([]);
+  const cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header cartItemCount={cartItemCount} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductListingPage cart={cart} setCart={setCart} />} />
+        <Route path="/cart" element={<CartPage cart={cart} setCart={setCart} />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
